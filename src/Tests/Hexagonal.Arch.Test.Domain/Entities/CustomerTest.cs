458f2a422b1799804d84e671ae6030b462e82e88
gabriel <gabriel.maurici@hotmail.com>
2024-06-11 21:21:32 -0300

    refatorado test Customer

--- src/Tests/Hexagonal.Arch.Test.Domain/Entities/CustomerTest.cs
+++ src/Tests/Hexagonal.Arch.Test.Domain/Entities/CustomerTest.cs
@@ -10,23 +10,18 @@ namespace Hexagonal.Arch.Test.Domain.Entities
         [Fact(DisplayName = "Return Customer when Customer is valid")]
         public void CustomerValid_WhenCustomerIsValid_ReturnCustomer()
         {
-            var document = "999.999.999-99";
-            var cpf = new Cpf(document);
-
-            var cep = "88999-999";
-            var street = "Rua Teste";
-            var district = "Bairro Teste";
-            var address = new Address(cep, street, district);
-
+            var cpf = new Cpf("999.999.999-99");
+            var address = new Address("88999-999", "Rua teste", "Bairro teste");
             var id = 0;
             var name = "Joãozinho";
             short? age = 18;
+            
             var customer = new Customer(id, name, age, cpf, address);
     
-            Assert.Equal(document, customer.Cpf.Document);
-            Assert.Equal(cep, customer.Address.Cep);
-            Assert.Equal(street, customer.Address.Street);
-            Assert.Equal(district, customer.Address.District);
+            Assert.Equal(cpf.Document, customer.Cpf.Document);
+            Assert.Equal(address.Cep, customer.Address.Cep);
+            Assert.Equal(address.Street, customer.Address.Street);
+            Assert.Equal(address.District, customer.Address.District);
             Assert.Equal(id, customer.Id);
             Assert.Equal(name, customer.Name);
             Assert.Equal(age, customer.Age);
@@ -35,14 +30,8 @@ namespace Hexagonal.Arch.Test.Domain.Entities
         [Fact(DisplayName = "Throw null Name exception when Name is null or empty")]
         public void NameIsNullOrEmpty_WhenNameIsNullOrEmpty_ThrowNullNameException()
         {
-            var document = "999.999.999-99";
-            var cpf = new Cpf(document);
-            
-            var cep = "88999-999";
-            var street = "Rua Teste";
-            var district = "Bairro Teste";
-            var address = new Address(cep, street, district);
-            
+            var cpf = new Cpf("999.999.999-99");
+            var address = new Address("88999-999", "Rua teste", "Bairro teste");            
             var id = 0;
             string? nameNull = null;
             var nameEmpty = string.Empty;
@@ -56,12 +45,7 @@ namespace Hexagonal.Arch.Test.Domain.Entities
         public void CpfIsNull_WhenCpfIsNull_ThrowNullCpfException()
         {
             Cpf? cpf = null;
-            
-            var cep = "88999-999";
-            var street = "Rua Teste";
-            var district = "Bairro Teste";
-            var address = new Address(cep, street, district);
-            
+            var address = new Address("88999-999", "Rua teste", "Bairro teste");            
             var id = 0;
             var name = "Joãozinho";
             short? age = 18;
@@ -73,9 +57,7 @@ namespace Hexagonal.Arch.Test.Domain.Entities
         public void AddressNull_WhenAdressIsNull_ThrowNullAddressException()
         {
             var cpf = new Cpf("999.999.999-99");
-
             Address? address = null;
-
             var id = 0;
             var name = "Joãozinho";
             short? age = 18;
