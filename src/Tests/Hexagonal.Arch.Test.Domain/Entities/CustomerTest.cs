941ed6d01bd4b4e34e1f6ad1609e85d411f30ecc
gabriel <gabriel.maurici@hotmail.com>
2024-06-11 21:16:57 -0300

    add test CustomerFactory e refatorado test Customer

--- src/Tests/Hexagonal.Arch.Test.Domain/Entities/CustomerTest.cs
+++ src/Tests/Hexagonal.Arch.Test.Domain/Entities/CustomerTest.cs
@@ -1,6 +1,7 @@
 using Hexagonal.Arch.Domain.Entities;
 using Hexagonal.Arch.Domain.Exceptions;
 using Hexagonal.Arch.Domain.ValueObjects;
+using Microsoft.VisualBasic;
 
 namespace Hexagonal.Arch.Test.Domain.Entities
 {
@@ -17,16 +18,16 @@ namespace Hexagonal.Arch.Test.Domain.Entities
             var district = "Bairro Teste";
             var address = new Address(cep, street, district);
 
+            var id = 0;
             var name = "Joãozinho";
             short? age = 18;
-            var customer = new Customer(name, age, cpf, address);
+            var customer = new Customer(id, name, age, cpf, address);
     
             Assert.Equal(document, customer.Cpf.Document);
-
             Assert.Equal(cep, customer.Address.Cep);
             Assert.Equal(street, customer.Address.Street);
             Assert.Equal(district, customer.Address.District);
-
+            Assert.Equal(id, customer.Id);
             Assert.Equal(name, customer.Name);
             Assert.Equal(age, customer.Age);
         }
@@ -42,12 +43,13 @@ namespace Hexagonal.Arch.Test.Domain.Entities
             var district = "Bairro Teste";
             var address = new Address(cep, street, district);
             
+            var id = 0;
             string? nameNull = null;
             var nameEmpty = string.Empty;
             short? age = 18;
             
-            Assert.Throws<NullNameException>(() => new Customer(nameNull!, age, cpf, address!));
-            Assert.Throws<NullNameException>(() => new Customer(nameEmpty, age, cpf, address!));
+            Assert.Throws<NullNameException>(() => new Customer(id, nameNull!, age, cpf, address!));
+            Assert.Throws<NullNameException>(() => new Customer(id, nameEmpty, age, cpf, address!));
         }
 
         [Fact(DisplayName = "Throw null Cpf exception when Cpf is null")]
@@ -60,21 +62,25 @@ namespace Hexagonal.Arch.Test.Domain.Entities
             var district = "Bairro Teste";
             var address = new Address(cep, street, district);
             
+            var id = 0;
             var name = "Joãozinho";
             short? age = 18;
             
-            Assert.Throws<NullCpfException>(() => new Customer(name, age, cpf!, address!));
+            Assert.Throws<NullCpfException>(() => new Customer(id, name, age, cpf!, address!));
         }
 
         [Fact(DisplayName = "Throw null Address exception when Addres is null")]
         public void AddressNull_WhenAdressIsNull_ThrowNullAddressException()
         {
-            var name = "Joãozinho";
-            short? age = 18;
             var cpf = new Cpf("999.999.999-99");
+
             Address? address = null;
 
-            Assert.Throws<NullAddressException>(() => new Customer(name, age, cpf, address!));
+            var id = 0;
+            var name = "Joãozinho";
+            short? age = 18;
+
+            Assert.Throws<NullAddressException>(() => new Customer(id, name, age, cpf, address!));
         }
     }
 }
---
+++ src/Tests/Hexagonal.Arch.Test.Domain/Factories/CustomerFactoryTest.cs
@@ -0,0 +1,29 @@
+using System.Data.Common;
+using Hexagonal.Arch.Domain.Entities;
+using Hexagonal.Arch.Domain.Factories;
+using Hexagonal.Arch.Domain.ValueObjects;
+using Microsoft.VisualBasic;
+
+namespace Hexagonal.Arch.Test.Domain.Factories;
+
+public class CustomerFactoryTest
+{
+    [Fact(DisplayName = "Return new Customer when create CustomerFactory valid")]
+    public void CustomerFactoryValid_WhenCustomerFactoryValid_ReturnNewCustomer() 
+    {
+        var id = 0;
+        var name = "Joãozinho";
+        short? age = 18;
+        var cpf = new Cpf("999.999.999-99");
+        var address = new Address("88999-999", "Rua Teste", "Bairro Teste");
+
+        var customer = CustomerFactory.Create(name, age, cpf, address);
+
+        Assert.IsType<Customer>(customer);
+        Assert.Equal(id, customer.Id);
+        Assert.Equal(name, customer.Name);
+        Assert.Equal(age, customer.Age);
+        Assert.Equal(cpf, customer.Cpf);
+        Assert.Equal(address, customer.Address);
+    }    
+}
